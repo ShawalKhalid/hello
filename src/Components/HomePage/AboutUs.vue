a3392709244e8a23f003e36533c3f92178ab7ca5
ShawalKhalid <40204753+ShawalKhalid@users.noreply.github.com>
2022-10-23 18:27:12 -0400

    Add files via upload

---
+++ src/Components/HomePage/AboutUs.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="about-us">
+
+    <div class="row p-4">
+      <div class="col-12 col-sm-6 text-sm-right text-md-right ml-sm-0 ml-md-5 ml-lg-5 ml-xl-5 text-lg-right order-sm-first order-12" style="margin-right: -8%; z-index:1;">
+        <h1>WE LOVE BOOKS</h1>
+        <h6>“If you only read the books that everyone else is reading,
+          <br>you can only think what everyone else is thinking.”
+          <br> Haruki Murakami
+        </h6>
+      </div>
+      <div class="col-6">
+        <img src="@/assets/33.jpg">
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AboutUs'
+}
+</script>
+
+<style scoped>
+.about-us img {
+  width: 90%;
+  min-width: 380px;
+}
+</style>
